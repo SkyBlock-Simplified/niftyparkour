0b4adab5f06433dadd3b5308e3c3a70107eb4735
Brian Graham <support@netcoding.net>
2016-01-18 21:05:00 -0500

    Send checkpoint sign update when toggling someones admin state

--- src/main/java/net/netcoding/niftyparkour/commands/AdminMode.java
+++ src/main/java/net/netcoding/niftyparkour/commands/AdminMode.java
@@ -4,6 +4,7 @@ import net.netcoding.niftybukkit.NiftyBukkit;
 import net.netcoding.niftybukkit.minecraft.BukkitCommand;
 import net.netcoding.niftybukkit.mojang.BukkitMojangProfile;
 import net.netcoding.niftycore.util.ListUtil;
+import net.netcoding.niftyparkour.NiftyParkour;
 import net.netcoding.niftyparkour.cache.UserParkourData;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -38,6 +39,7 @@ public class AdminMode extends BukkitCommand {
 		UserParkourData userData = UserParkourData.getCache(profile);
 		userData.toggleAdminMode();
 		this.getLog().message(sender, "{{0}} is no{1} in admin mode.", profile.getName(), (userData.isAdminMode() ? "w" : " longer"));
+		NiftyParkour.sendCheckpointSignUpdate(profile);
 	}
 
 }
