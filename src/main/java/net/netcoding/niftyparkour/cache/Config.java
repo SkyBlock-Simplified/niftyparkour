e014620de0a3cb7529123f26496618a8a4ea82d9
Brian Graham <support@netcoding.net>
2016-04-15 02:58:55 -0400

    Added menu item opener

--- src/main/java/net/netcoding/niftyparkour/cache/Config.java
+++ src/main/java/net/netcoding/niftyparkour/cache/Config.java
@@ -1,8 +1,13 @@
 package net.netcoding.niftyparkour.cache;
 
+import net.netcoding.niftybukkit.NiftyBukkit;
+import net.netcoding.niftybukkit.inventory.items.ItemData;
 import net.netcoding.niftybukkit.util.LocationUtil;
 import net.netcoding.niftybukkit.yaml.BukkitConfig;
+import net.netcoding.niftycore.yaml.annotations.Comment;
 import net.netcoding.niftycore.yaml.annotations.Path;
+import net.netcoding.niftycore.yaml.exceptions.InvalidConfigurationException;
+import net.netcoding.niftyparkour.NiftyParkour;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,21 +17,43 @@ public class Config extends BukkitConfig {
 	public static final Location DEFAULT_SPAWN = Bukkit.getWorlds().get(0).getSpawnLocation();
 
 	@Path("spawn-point")
+	@Comment("Where players spawn server wide")
 	private Location spawnPoint = DEFAULT_SPAWN;
 
 	@Path("fire-burn-duration")
+	@Comment("How long a player will burn for in seconds")
 	private Integer fireBurnDuration = 3;
 
+	@Path("item-opener")
+	@Comment("Adds item to your inventory to open the chest of servers")
+	private ItemData itemOpener = NiftyBukkit.getItemDatabase().get("0");
+
 	public Config(JavaPlugin plugin) {
-		super (plugin.getDataFolder(), "config.yml");
+		super (plugin.getDataFolder(), "config");
 	}
 
 	public int getFireBurnDuration() {
 		return this.fireBurnDuration + 1;
 	}
 
+	public ItemData getItemOpener() {
+		return this.itemOpener;
+	}
+
 	public Location getSpawnPoint() {
 		return this.spawnPoint;
+	}
+
+	@Override
+	public void reload() throws InvalidConfigurationException {
+		super.reload();
+
+		if (NiftyParkour.getMenuInventory() != null) {
+			NiftyParkour.getMenuInventory().closeAll();
+			NiftyParkour.getMenuInventory().setAutoCancelled();
+			NiftyParkour.getMenuInventory().setTitle("Maps");
+			NiftyParkour.getMenuInventory().setItemOpener(this.getItemOpener());
+		}
 	}
 
 	public void setSpawnPoint(Location location) {
