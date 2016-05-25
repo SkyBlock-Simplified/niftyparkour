431f084ab5995eba5a415661956e96ae11b69b1d
Brian Graham <support@netcoding.net>
2016-05-24 20:47:20 -0400

    Event name change

--- src/main/java/net/netcoding/niftyparkour/listeners/Connections.java
+++ src/main/java/net/netcoding/niftyparkour/listeners/Connections.java
@@ -1,8 +1,8 @@
 package net.netcoding.niftyparkour.listeners;
 
 import net.netcoding.niftybukkit.minecraft.BukkitListener;
-import net.netcoding.niftybukkit.minecraft.events.PlayerDisconnectEvent;
-import net.netcoding.niftybukkit.minecraft.events.PlayerPostLoginEvent;
+import net.netcoding.niftybukkit.minecraft.events.profile.ProfileJoinEvent;
+import net.netcoding.niftybukkit.minecraft.events.profile.ProfileQuitEvent;
 import net.netcoding.niftyparkour.cache.UserParkourData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,13 +14,13 @@ public class Connections extends BukkitListener {
 	}
 
 	@EventHandler
-	public void onPlayerPostLogin(PlayerPostLoginEvent event) {
+	public void onProfileJoin(ProfileJoinEvent event) {
 		UserParkourData userData = new UserParkourData(this.getPlugin(), event.getProfile());
 		userData.teleportToSpawn();
 	}
 
 	@EventHandler
-	public void onPlayerDisconnect(PlayerDisconnectEvent event) {
+	public void onProfileQuit(ProfileQuitEvent event) {
 		UserParkourData.removeCache(event.getProfile());
 	}
 
